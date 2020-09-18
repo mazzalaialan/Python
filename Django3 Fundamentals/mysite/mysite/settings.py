e6103670566d4294f2ce04de2a424a7a3173dcbe
Alan Dario Mazzalai <mazzalaialan@gmail.com>
2020-09-18 15:18:10 -0300

    sk

--- Django3 Fundamentals/mysite/mysite/settings.py
+++ Django3 Fundamentals/mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'aqa%of2i+@(md1jr2j1ge-(%v0cokyq)4k(t2ljl59e!h^k^=1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
