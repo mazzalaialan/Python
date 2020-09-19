e81f6bb548508f1e5a2ecf9592743c2900800a3d
Alan Dario Mazzalai <mazzalaialan@gmail.com>
2020-09-19 20:38:00 -0300

    decouple

--- Django/counter/counter/settings.py
+++ Django/counter/counter/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'cv8_-@u0zhfb*!n5b&d!w^qkuerlw4xk11!$zqi(=&kls*(@1+'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'counter.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / config('DATABASE_FILE'),
     }
 }
 
